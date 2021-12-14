885cfb7b97a160d90e84aa09367e840f2e1af0c7
RachitaN <rachitan.is19@bmsce.ac.in>
2021-12-15 00:06:55 +0530

    login

--- app/src/main/java/com/example/healpro/MainActivity.java
+++ app/src/main/java/com/example/healpro/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.healpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
@@ -10,16 +14,26 @@ import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
     CarouselView carouselView;
-
-    int[] sampleImages = {R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground};
+    Button login,register;
+    int[] sampleImages = {R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
 
         carouselView.setImageListener(imageListener);
+        login=findViewById(R.id.button2);
+        register=findViewById(R.id.button3);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity1Intent = new Intent(getApplicationContext(), login.class);
+                startActivity(activity1Intent);
+            }
+        });
     }
 
     ImageListener imageListener = new ImageListener() {
---
+++ app/src/main/java/com/example/healpro/login.java
@@ -0,0 +1,43 @@
+package com.example.healpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+    TextView marq;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        Animation anim= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide);
+        ImageView img=findViewById(R.id.heart);
+        img.startAnimation(anim);
+        anim.setRepeatCount(Animation.INFINITE);
+        Button login=findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText n=(EditText)findViewById(R.id.editTextTextPersonName2);
+                EditText pd=(EditText)findViewById(R.id.editTextTextPassword2);
+                String name=n.getText().toString();
+                String password=pd.getText().toString();
+                if(name.equals("user") && password.equals("user")){
+                Intent activity2Intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(activity2Intent);}
+                else{
+                    Toast.makeText(getApplicationContext(), "Invalid credentials, please verify!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
