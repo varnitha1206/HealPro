cd860341a9a7009ccc47efa2cc49e6300b8b8a56
RachitaN <rachitan.is19@bmsce.ac.in>
2021-12-15 19:12:34 +0530

    register

--- app/src/main/java/com/example/healpro/Register.java
+++ app/src/main/java/com/example/healpro/Register.java
@@ -6,29 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
-    Button reg;
-    EditText nam,username;
+    Button reg,verify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        reg=(Button) findViewById(R.id.register2);
-        nam=(EditText) findViewById(R.id.edit_name);
-        String n=nam.getText().toString();
-        username=(EditText)findViewById(R.id.username);
-        String uname=username.getText().toString();
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nam,username,;
+                reg=(Button) findViewById(R.id.register2);
+                nam=(EditText) findViewById(R.id.edit_name);
+                String n=nam.getText().toString();
+                username=(EditText)findViewById(R.id.edit_username);
+                String uname=username.getText().toString();
+
+            }
+        });
             reg.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    if (!n.matches("") && !uname.matches(""))
-                    {
                         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(peekAvailableContext());
                         alertDialogBuilder.setTitle("Registration");
                         alertDialogBuilder.setIcon(R.drawable.logo);
@@ -48,11 +53,6 @@ public class Register extends AppCompatActivity {
                         alertDialogBuilder.setCancelable(false);
                         AlertDialog alertdialog = alertDialogBuilder.create();
                         alertdialog.show();
-                    }
-                    else
-                        {
-                        Toast.makeText(getApplicationContext(), "Please enter details to proceed", Toast.LENGTH_SHORT).show();
-                    }
                 }
             });
 
